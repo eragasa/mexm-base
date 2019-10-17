f131af1593e5b7477eb4b2a88dcd03842b509b92
Eugene Ragasa <eragasa@users.noreply.github.com>
2019-10-17 18:46:24 -0400

    io.slurm.SlurmConfiguration added some integration tests

--- tests/io/slurm/SlurmConfiguration/test__SlurmConfiguration.py
+++ tests/io/slurm/SlurmConfiguration/test__SlurmConfiguration.py
@@ -21,6 +21,14 @@ slurm_vasp_configuration_dict = OrderedDict([
     ('modules', ['intel/2016.0.19', 'impi']),
     ('run_string', 'srun --mpi=pmi2 $VASP_BIN > vasp.log')
 ])
+slurm_phonts_configuration_dict = OrderedDict([
+    ('modules', ['intel/2016.0.19', 'impi']),
+    ('run_string', 'srun --mpi=pmi2 $PHONTS_BIN > phonts.log')
+])
+slurm_set_lammps_configuration_dict = OrderedDict([
+    ('modules', ['openmpi']),
+    ('run_string', 'srun --mpi=pmi2 $LAMMPS_SERIAL_BIN > lammps.out')
+])
 
 def dev____init____no_args():
     slurm_configuration = SlurmConfiguration()
@@ -29,22 +37,36 @@ def dev____init____no_args():
 def test____init____no_args():
     slurm_configuration = SlurmConfiguration()
 
+def dev__SlurmConfiguration():
+    o = SlurmConfiguration()
+    o.set_slurm_configuration(
+        job_name='default_job',
+        qos='phillpot-b',
+        mail_type='END',
+        mail_name='eragasa@ufl.edu',
+        ntasks='16',
+        output_path='job.out',
+        error_path='job.err',
+        time='1:00:00',
+        memory='4gb'
+    )
+    o.set_vasp_configuration(
+        modules=['intel/2016.0.19', 'impi'],
+        run_string='srun --mpi=pmi2 $VASP_BIN > vasp.log'
 
-slurm_configuration = SlurmConfiguration()
-slurm_configuration.set_vasp_configuration(
-    modules=['intel/2016.0.19', 'impi'],
-    run_string='srun --mpi=pmi2 $VASP_BIN > vasp.log'
+    )
+    o.set_phonts_configuration(
+        modules=['intel/2016.0.19', 'impi'],
+        run_string='srun --mpi=pmi2 $PHONTS_BIN > phonts.log'
+    )
+    o.set_lammps_configuration(
+        modules=['openmpi'],
+        run_string='srun --mpi=pmi2 $LAMMPS_SERIAL_BIN > lammps.out'
+    )
 
-)
-slurm_configuration.set_phonts_configuration(
-    modules=['intel/2016.0.19', 'impi'],
-    run_string='srun --mpi=pmi2 $PHONTS_BIN > phonts.log'
-)
-slurm_configuration.set_lammps_configuration(
-    modules=['openmpi'],
-    run_string='srun --mpi=pmi2 $LAMMPS_SERIAL_BIN > lammps.out'
-)
+    slurm_configuration.write('slurm_config.yaml')
+    slurm_configuration.read('slurm_config.yaml')
+    os.remove(slurm_config_path)
 
-slurm_configuration.write('slurm_config.yaml')
-slurm_configuration.read('slurm_config.yaml')
-os.remove(slurm_config_path)
+if __name__ == "__main__":
+    dev__SlurmConfiguration()
