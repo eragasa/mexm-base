4f4a6b6e79b64142df8310fbb43d4f12e69c0375
Eugene Ragasa <eragasa@users.noreply.github.com>
2019-10-27 16:40:10 -0400

    removed old code cruft from the Qoi base class

--- mexm/qoi/__init__.py
+++ mexm/qoi/__init__.py
@@ -1,3 +1,5 @@
 from mexm.qoi.base_qoi import Qoi
-from mexm.qoi.elastic import ElasticPropertyCalculations
-from mexm.qoi.lammps_elastic import LammpsElasticPropertyCalculations
+
+from mexm.qoi.elastic import ElasticProperties
+from mexm.qoi.lammps_elastic import LammpsElasticProperties
+from mexm.qoi.gulp import GulpElasticProperties
