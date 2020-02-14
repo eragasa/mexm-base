22cedcf12d28b1765fa6633cdbcc7550e9179644
Eugene J. Ragasa <eragasa@ufl.edu>
2020-02-14 17:10:08 -0500

    sniped my pypi name

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="mexm",
+      version="0.1.0",
+      description="Materials Ex Machina",
+      url="https://github.com/eragasa/mexm-base",
+      author="Eugene J. Ragasa",
+      author_email="eragasa@osc.edu",
+      license="MIT License",
+      packages=["mexm",],
+      zip_safe=True)
