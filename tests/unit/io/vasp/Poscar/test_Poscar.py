9481570566b2829dbeaa6370a365cb29c7ca9219
Eugene Ragasa <eragasa@users.noreply.github.com>
2020-02-20 05:27:59 -0500

    0527 tests for io.vasp.Poscar

--- tests/unit/io/vasp/Poscar/test_Poscar.py
+++ tests/unit/io/vasp/Poscar/test_Poscar.py
@@ -4,39 +4,37 @@ import os
 import numpy as np
 from mexm.io.vasp import Poscar
 
+parent_path = os.path.dirname(os.path.abspath(__name__))
+
 def test_Poscar____init__():
     Poscar()
 
-def test_Poscar__read():
-    poscar_path = 'Cu_fcc.vasp'
+def test_read():
+    poscar_path = os.path.join(
+        parent_path, 'resources', 'Si_primitive.vasp'
+    )
+
     o = Poscar()
     o.read(path=poscar_path)
 
-def test_Poscar__read__hmatrix():
-    poscar_path = 'hmatrixtest.vasp'
+def test_read__check_h_matrix():
+    poscar_path = os.path.join(parent_path, 'resources', 'hmatrixtest.vasp')
+
     o = Poscar()
     o.read(path=poscar_path)
 
-    expected_h_matrix = np.array([
-        [1.,2.,3.],
-        [4.,5.,6.],
-        [7.,8.,9.]
-    ]).T
+    h_matrix = np.array([[1.,2.,3.],[4.,5.,6.], [7.,8.,9.]]).T
     assert np.array_equal(o.H, expected_h_matrix)
 
-def test_Poscar__read__latticevectors():
-    poscar_path = 'hmatrixtest.vasp'
+def test_read__check_lattice_vectors():
+    poscar_path = os.path.join(parent_path, 'resources', 'hmatrixtest.vasp')
+    h_matrix = np.array([[1.,2.,3.],[4.,5.,6.],[7.,8.,9.]].T
+
     o = Poscar()
     o.read(path=poscar_path)
-
-    expected_h_matrix = np.array([
-        [1.,2.,3.],
-        [4.,5.,6.],
-        [7.,8.,9.]
-    ]).T
-    assert np.array_equal(o.a1, expected_h_matrix[:,0])
-    assert np.array_equal(o.a2, expected_h_matrix[:,1])
-    assert np.array_equal(o.a3, expected_h_matrix[:,2])
+    assert np.array_equal(o.a1, h_matrix[:,0])
+    assert np.array_equal(o.a2, h_matrix[:,1])
+    assert np.array_equal(o.a3, h_matrix[:,2])
 
 def dev_Poscar__read():
     print(80*'-')
@@ -55,17 +53,21 @@ def dev_Poscar__read():
     print('a2:',o.a2)
     print('a3:',o.a3)
 
-def test_Poscar__write():
-    src_poscar_path = 'Cu_fcc.vasp'
+@pytest.fixture
+def src_poscar_path():
+    poscar_name = 'Si_primitive.vasp'
+    return os.path.join(parent_path, 'resources', poscar_name)
+
+def test_write(src_poscar_path):
     dst_poscar_path = 'POSCAR'
 
     o = Poscar()
     o.read(path=src_poscar_path)
     o.write(path=dst_poscar_path)
 
-def dev_Poscar__write():
-    src_poscar_path = 'hmatrixtest.vasp'
-    dst_poscar_path = 'POSCAR'
+def dev__write():
+    src_path = os.path.join(parent_path, 'resources', 'Si_primitive.vasp')
+    dst_path = 'POSCAR'
 
     o = Poscar()
     o.read(path=src_poscar_path)
