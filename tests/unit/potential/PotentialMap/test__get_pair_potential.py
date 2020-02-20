6915a76c531d34f3f8d0e17bc064339f3813a766
Eugene Ragasa <eragasa@users.noreply.github.com>
2020-02-20 03:07:59 -0500

    remove pypospack dependencies 0307

--- tests/unit/potential/PotentialMap/test__get_pair_potential.py
+++ tests/unit/potential/PotentialMap/test__get_pair_potential.py
@@ -1,6 +1,6 @@
 import pytest
-from pypospack.potential import PairPotential, pair_potential_names
-from pypospack.potential.loader import get_pair_potential
+from mexm.potential import PairPotential, pair_potential_names
+from mexm.potential.loader import get_pair_potential
 
 @pytest.mark.parametrize('name,symbols',[(k,['Ni']) for k in pair_potential_names])
 def test__get_pair_potential(name,symbols):
