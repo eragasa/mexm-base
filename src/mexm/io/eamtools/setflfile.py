36c9254e9c8332864f87a4f77c5239ae7401369e
Eugene Ragasa <eragasa@users.noreply.github.com>
2020-02-20 16:09:34 -0500

    0409 io.eamtools.setflfile bugfix

--- src/mexm/io/eamtools/setflfile.py
+++ src/mexm/io/eamtools/setflfile.py
@@ -1,5 +1,5 @@
-from mexm.io.eamtools import SetflReader, SetflWriter
-from mexm.potential import get_symbol_pairs
+from mexm.io.eamtools import SetflReader 
+from mexm.io.eamtools import SetflWriter
 
 class SetflFile():
     """ class for the setfl
@@ -11,25 +11,39 @@ class SetflFile():
 
     @property
     def src_path(self):
+        """:str path to read the setflfile"""
         return self.src_file.path
 
     @src_path.setter
     def src_path(self, path):
-        assert isinstance(path, str)
-
         self.src_file.path = path
 
     @property
     def dst_path(self):
+        """:str path to write the setflfile"""
         return self.dst_file.path
 
     def read(self, path=None):
+        """ read the setfl file
+
+        Args:
+            path (str): path to read the setflfile
+        """
+
+        # handle method arguments
         if path is not None:
             assert isinstance(path, str)
             self.src_path = path
+
+        # read the file using the SetflReader
         self.src_file.read(path=self.src_path)
 
     def write(self, path=None):
+        """ write the setfl file
+
+        Args:
+            path (str): path to write the setflfile
+        """
         if path is not None:
             assert isinstance(path, None)
             self.dst_path = path
