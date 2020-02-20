410f8a62845b21d90bd2693b17f866c8742b0d41
Eugene Ragasa <eragasa@users.noreply.github.com>
2020-02-19 23:34:20 -0500

    travis 11:34pm try\

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+test_deps = [
+    'coverage',
+    'pytest',
+]
+
+
 setup(name="mexm",
       version="0.1.0",
       description="Materials Ex Machina",
@@ -8,4 +14,5 @@ setup(name="mexm",
       author_email="eragasa@osu.edu",
       license="MIT License",
       packages=["src/mexm", find_packages()],
+      tests_require=test_deps,
       zip_safe=True)
